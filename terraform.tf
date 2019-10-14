f6df983448fb344b7bab46fe2ced75c6bdfcf4d9
Ondrej Sika <ondrej@ondrejsika.com>
2019-10-14 14:40:49 +0200

    Update Kubernetes version

--- terraform.tf
+++ terraform.tf
@@ -16,7 +16,7 @@ provider "cloudflare" {
 resource "digitalocean_kubernetes_cluster" "sikademo" {
   name    = "sikademo"
   region  = "fra1"
-  version = "1.15.3-do.2"
+  version = "1.15.4-do.0"
 
   node_pool {
     name       = "sikademo"
