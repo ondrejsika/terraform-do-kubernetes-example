c680fc7f2b8fd679545c4275dcb2c8e757fd911d
Ondrej Sika <ondrej@ondrejsika.com>
2019-11-12 09:46:41 +0100

    Upgrade Kubernetes version to 1.15.5-do.1

--- terraform.tf
+++ terraform.tf
@@ -16,7 +16,7 @@ provider "cloudflare" {
 resource "digitalocean_kubernetes_cluster" "sikademo" {
   name    = "sikademo"
   region  = "fra1"
-  version = "1.15.4-do.0"
+  version = "1.15.5-do.1"
 
   node_pool {
     name       = "sikademo"
